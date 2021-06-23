602acf0fe6a260be214e5cb4add585715b23c31e
thebaske <thebaske@gmail.com>
2021-06-23 09:43:36 +0200

    Materials push fix
    Fixing mats

---
+++ Assets/_01Scripts/OnTriggerActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerActions : MonoBehaviour
+{
+    public UnityEvent OnTriggerEnterAction;
+    public UnityEvent OnTriggerExitAction;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+            OnTriggerEnterAction?.Invoke();
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+            OnTriggerExitAction?.Invoke();
+        
+    }
+}
